c9d0b880f752c0c083351d8666ab666beef256e6
Mandana Dashti <dmandana59@gmail.com>
2025-11-30 08:33:07 +0330

    fix: add weekDisplay

--- src/components/Calender.tsx
+++ src/components/Calender.tsx
@@ -1,17 +1,28 @@
 import {useState} from "react";
-import MonthDisplay from "./MonthDisplay.tsx";
+import MonthDisplay from "./Month/MonthDisplay.tsx";
 import CalenderHeader from "./CalenderHeader.tsx";
+import type {ViewMode} from "../types/calendar.types.ts";
+import WeekDisplay from "./Week/WeekDisplay.tsx";
 
 
 const Calender = () => {
     const today = new Date();
-
     const [currentDate, setCurrentDate] = useState(today);
+    const [view, setView] = useState<ViewMode>("month");
 
     return (
         <div className={"w-full space-y-4"}>
-            <CalenderHeader today={today} currentDate={currentDate} setCurrentDate={setCurrentDate}/>
-            <MonthDisplay today={today} currentDate={currentDate}/>
+            <CalenderHeader setView={setView} view={view} today={today} currentDate={currentDate}
+                            setCurrentDate={setCurrentDate}/>
+            {
+                view === "month" ? (
+                    <MonthDisplay today={today} currentDate={currentDate}/>
+                ) : (
+                    view === "week" && (
+                        <WeekDisplay today={today} currentDate={currentDate}/>
+                    )
+                )
+            }
         </div>
     )
 }
