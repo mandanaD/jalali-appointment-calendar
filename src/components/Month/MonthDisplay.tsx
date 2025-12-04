dc553d95e67cf48a07885b38cd9e572848b814fa
Mandana Dashti <dmandana59@gmail.com>
2025-12-04 23:10:18 +0330

    fix: add isSame func

--- src/components/Month/MonthDisplay.tsx
+++ src/components/Month/MonthDisplay.tsx
@@ -1,5 +1,5 @@
-import {eachDayOfInterval, endOfMonth, endOfWeek, format, startOfMonth, startOfWeek} from "date-fns-jalali";
-import {gregorianMonths, WEEK_DAYS} from "../constans/calender.ts";
+import {eachDayOfInterval, endOfMonth, endOfWeek, format, isSameDay, startOfMonth, startOfWeek} from "date-fns-jalali";
+import {gregorianMonths, WEEK_DAYS} from "../../constans/calender.ts";
 
 const MonthDisplay = ({currentDate, today}: {
     currentDate: Date;
@@ -26,8 +26,7 @@ const MonthDisplay = ({currentDate, today}: {
             {gridDays.map((day) => {
                 const isCurrentMonth =
                     format(day, "MM") === format(currentDate, "MM");
-                const isCurrentDay =
-                    format(day, "dd") === format(today, "dd");
+                const isToday=isSameDay(day,today)
 
                 return (
                     <div
@@ -35,7 +34,7 @@ const MonthDisplay = ({currentDate, today}: {
                         className={`
           h-24 border border-gray-300/60 p-1 text-sm flex justify-between
           ${isCurrentMonth ? "" : "bg-gray-100 text-gray-400"}
-          ${(today.toISOString().slice(0, 10)===currentDate.toISOString().slice(0, 10))&&isCurrentDay ? "!bg-blue-100 !text-blue-400" : ""}
+          ${isToday ? "!bg-blue-100 !text-blue-400" : ""}
         `}>
                         <span>
                         {format(day, "dd")}
