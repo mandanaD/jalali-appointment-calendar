b635e19f69ffbb28a0264ed5975a5d90c14e7852
Mandana Dashti <dmandana59@gmail.com>
2025-12-11 21:58:38 +0330

    feat: create JALALI_MONTH

--- src/constans/calender.ts
+++ src/constans/calender.ts
@@ -12,3 +12,18 @@ export const WEEK_DAYS = [
     {key: "thursday", name: "پنجشنبه", short: "پ"},
     {key: "friday", name: "جمعه", short: "ج"},
 ];
+
+export const JALALI_MONTH = [
+    {value: "1", label: "فروردین"},
+    {value: "2", label: "اردیبهشت"},
+    {value: "3", label: "خرداد"},
+    {value: "4", label: "تیر"},
+    {value: "5", label: "مرداد"},
+    {value: "6", label: "شهریور"},
+    {value: "7", label: "مهر"},
+    {value: "8", label: "آبان"},
+    {value: "9", label: "آذر"},
+    {value: "10", label: "دی"},
+    {value: "11", label: "بهمن"},
+    {value: "12", label: "اسفند"},
+];
